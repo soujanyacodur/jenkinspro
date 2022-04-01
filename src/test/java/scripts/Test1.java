3f124b9910c732591977aaba6bc1b7cb263763ba
souja_aeuhg5j <souja_aeuhg5j@DESKTOP-MR4P3OJ>
2022-03-31 21:44:57 -0400

    hi

---
+++ src/test/java/scripts/Test1.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void demo() {
+		Reporter.log("Hello Java",true);
+		
+	}
+
+}
